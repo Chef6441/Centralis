3904260fc88b3527d7abd1f6163f89b8a72f21c4
Chef6441 <support@evergy.com.au>
2025-09-09 23:42:13 +1000

    added relationship for schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE company (
+    company_id INTEGER PRIMARY KEY AUTOINCREMENT,
     company_name TEXT,
     abn TEXT,
     nmi_mirn TEXT,
@@ -8,11 +9,14 @@ CREATE TABLE company (
 );
 
 CREATE TABLE invoice (
+    invoice_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
     period TEXT,
     nmi_number TEXT,
     location2 TEXT,
     invoice TEXT,
     ex_gst REAL,
     gst REAL,
-    total REAL
+    total REAL,
+    FOREIGN KEY (company_id) REFERENCES company(company_id)
 );
